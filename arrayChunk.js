51d3371d6b26fa3185f63ab38c24f344a4e598bf
zayn <ltlehro@gmail.com>
2025-08-04 23:03:17 +0500

    completed chunked array challenge

---
+++ arrayChunk.js
@@ -0,0 +1,14 @@
+const chunk = (array, size) => {
+    chunked = []
+    index = 0
+    while (index < array.length) {
+        const chunk = array.slice(index, index+size)
+        chunked.push(chunk)
+        index += size
+    }
+    return chunked
+}
+
+const array = [1,2,3,4,5,6,7,8,9]
+
+console.log(chunk(array, 3))
