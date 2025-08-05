a77fb72b9ad485b3058df4218c447e70909bd38c
zayn <ltlehro@gmail.com>
2025-08-05 15:01:33 +0500

    created shift method for linked list

--- linkedList.js
+++ linkedList.js
@@ -64,12 +64,35 @@ class LinkedList {
         this.length++
         return this
     }
+
+    shift() {
+        if (!this.head) {
+            return undefined
+        }
+
+        let temp = this.head
+        this.head = this.head.next
+        temp.next = null
+        this.length--
+
+        if (this.length == 0 ) {
+            this.tail = null
+        }
+        
+        return temp
+    }
 }
 
 const myLinkedList = new LinkedList(1)
 
 myLinkedList.push(2)
 
+console.log(myLinkedList)
+
 myLinkedList.unshift(3)
+
+console.log(myLinkedList)
+
+myLinkedList.shift()
 
 console.log(myLinkedList)
