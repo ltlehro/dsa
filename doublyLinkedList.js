48e91a4c148659aea2eba172f8a227db8696c591
zayn <ltlehro@gmail.com>
2025-08-05 17:35:20 +0500

    created push, pop and unshirt methods for doubly linked list

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -13,4 +13,58 @@ class DoublyLinkedList {
         this.tail = this.head
         this.length = 1
     }
+
+    push(value) {  
+        let node = new Node(value)
+
+        if (!this.head) {
+            this.head = node
+            this.tail = this.head
+            this.length++
+            return this
+        }
+
+        this.tail.next = node
+        node.prev = this.tail
+        this.tail = node
+        this.length++
+        return this
+    }
+
+    pop() {
+        if(this.length==0) {
+            return undefined
+        }
+
+        let temp = this.tail
+
+        this.tail = this.tail.prev
+        this.tail.next = null
+        temp.prev = null
+        this.length--
+        return this
+    }
+
+    unshift(value) {
+        let node = new Node(value)
+        if (this.length==0) {
+            this.head = node
+            this.tail = node
+            return this
+        }
+
+        node.next = this.head
+        this.head.prev = node
+        this.head = node
+        this.length++
+        return this
+    }
 }
+
+const newDoublyLinkedList = new DoublyLinkedList(1)
+
+newDoublyLinkedList.push(2)
+
+newDoublyLinkedList.unshift(10)
+
+console.log(newDoublyLinkedList)
