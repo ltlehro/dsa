5acaef0272166a90cf86ef826d18d5be93eab4b8
zayn <ltlehro@gmail.com>
2025-08-05 16:58:03 +0500

    created a doubly linked list

---
+++ doublyLinkedList.js
@@ -0,0 +1,16 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
+
+class DoublyLinkedList {
+    constructor(value) {
+        let node = new Node(value)
+        this.head = node
+        this.tail = this.head
+        this.length = 1
+    }
+}
