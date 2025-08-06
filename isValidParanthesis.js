b529c71abf583f1258fc761dee99401e1ebae9ac
zayn <ltlehro@gmail.com>
2025-08-06 15:43:17 +0500

    completed isValidParanthesis challenge

---
+++ isValidParanthesis.js
@@ -0,0 +1,27 @@
+const isValidParanthesis = (str) => {
+    const stack = []
+
+    const brackets = {
+        "(" : ")",
+        "{" : "}",
+        "[" : "]",
+    }
+
+    for (let char of str) {
+        if (brackets[char]) {
+            stack.push(char)
+        } else {
+        const top = stack.pop()
+
+        if (!top || brackets[top] !== char) {
+            return false
+        }
+      }
+    }
+    return stack.length == 0
+}
+
+console.log(isValidParanthesis("()[]{}"))
+console.log(isValidParanthesis("{}[()"))
+console.log(isValidParanthesis("}[]()"))
+
