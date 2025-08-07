0f378470a9c89e2efb17a2d924eae36163d55491
zayn <ltlehro@gmail.com>
2025-08-07 13:11:51 +0500

    implemented dfs in order for binary search tree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -101,16 +101,26 @@ class BST {
 
         return data
     }
-}
 
+    dfsInOrder(node = this.root , data = []){
+
+        if (node == null) return data
+
+        if (node.left) this.dfsInOrder(node.left, data)
+        data.push(node.value)
+        if (node.right) this.dfsInOrder(node.right, data)
+
+        return data
+    }
+}
 
 const tree = new BST()
 
-tree.insert(1)
 tree.insert(2)
-tree.insert(3)
 tree.insert(4)
-tree.insert(5)
+tree.insert(6)
+tree.insert(8)
+tree.insert(10)
 
 console.log(tree)
 
