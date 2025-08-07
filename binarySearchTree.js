d3c754e8a3ca352ec8e8ea80c1b5c42fa88b2078
zayn <ltlehro@gmail.com>
2025-08-07 11:47:48 +0500

    created a binary search tree and implemented insert method

---
+++ binarySearchTree.js
@@ -0,0 +1,51 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null
+    }
+
+    insert(value) {
+        const newNode = new Node(value)
+        if (this.root === null) {
+            this.root = newNode
+            return this
+        }
+
+        let temp = this.root
+
+        while (true) {
+            if (newNode.value === temp.value) {
+                return undefined
+            }
+
+            if (newNode.value < temp.value) {
+                if (temp.left === null) {
+                    temp.left = newNode
+                    return this
+                }
+                temp = temp.left
+            } else {
+                if (temp.right === null) {
+                    temp.right = newNode
+                    return this
+                }
+                temp = temp.right
+            }
+        }
+    }
+}
+
+const tree = new BST()
+
+tree.insert(1)
+tree.insert(2)
+tree.insert(3)
+
+console.log(tree)
