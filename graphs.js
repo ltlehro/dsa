d83518d4d4fe37c345180f6455dfebb18cd96f6e
zayn <ltlehro@gmail.com>
2025-08-07 13:31:30 +0500

    created a method to remove edges from the adjacency list between two vertices

--- graphs.js
+++ graphs.js
@@ -19,6 +19,15 @@ class Graph {
         }
         return false
     }
+
+    removeEdge(vtx1, vtx2) {
+        if (this.adjacencyList[vtx1] && this.adjacencyList[vtx2]) {
+            this.adjacencyList[vtx1] = this.adjacencyList[vtx2].filter(v => v !== vtx1)
+            this.adjacencyList[vtx2] = this.adjacencyList[vtx1].filter(v => v !== vtx2)
+            return true
+        }
+        return false
+    }
 }
 
 const graph = new Graph()
@@ -28,5 +37,9 @@ graph.addVertex("B")
 graph.addVertex("C")
 
 graph.addEdge("A","B")
+
+console.log(graph)
+
+graph.removeEdge("A", "B")
 
 console.log(graph)
